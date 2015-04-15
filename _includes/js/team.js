df3046c51372e86120a8e7a702fd360a49e2af33
Chris Frank <chris@thenepotist.com>
2015-04-14 22:11:37 -0400

    better timeout

--- _includes/js/team.js
+++ _includes/js/team.js
@@ -1,5 +1,6 @@
-$(document).pjax('.people .inline-nav-item', '#people', {
-  fragment: '#people'
+$(document).pjax('.team .inline-nav-item', '#people', {
+  fragment: '#people',
+  timeout: 5000
 }).on('pjax:click', function(event) {
   $(event.target).addClass('active').siblings().removeClass('active')
 }).on('pjax:send', function(){
