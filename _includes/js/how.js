5e16e9f59df33185e8520dba2c26bd42492b06b3
Chris Frank <chris@thenepotist.com>
2015-04-24 12:51:49 -0400

    scrolling instructions are good

--- _includes/js/how.js
+++ _includes/js/how.js
@@ -1,8 +1,23 @@
-$('.scrolling-slide').waypoint({
-  handler: function(direction) {
-    $target = direction == 'down' ? $(this.element) : $(this.element).prev()
-    $('body').removeClass('slide-step-1 slide-step-2 slide-step-3').addClass('scrolling-slideshow slide-step-' + $target.attr('data-step'))
-    $target.addClass('active').siblings().removeClass('active')
-  },
-  offset: '50%'
-})
+$.fn.closeScrollingSlide = function(){
+  return this.each(function(){
+    $t = $(this)
+    $t.removeClass('active')
+  })
+}
+$.fn.openScrollingSlide = function(){
+  return this.each(function(){
+    $('body').removeClass('bg-step-1 bg-step-2 bg-step-3').addClass('scrolling-slideshow bg-step-' + this.getAttribute('data-step'))
+    $t = $(this)
+    $t.addClass('active')
+    $t.siblings('.scrolling-slide').closeScrollingSlide()
+  })
+}
+if (!Modernizr.touch) {
+  $('.scrolling-slide').waypoint({
+    handler: function(direction) {
+      $target = direction == 'down' ? $(this.element) : $(this.element).prev()
+      $target.openScrollingSlide()
+    },
+    offset: '50%'
+  })
+}
